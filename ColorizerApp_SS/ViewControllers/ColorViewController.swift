7b94b1c4da5be24045823780d8cefe0ac5319fbe
Sergey Sokolov <sergey.and.sokolov@gmail.com>
2022-11-20 13:35:22 +0400

    2.0

--- ColorizerApp_SS/ViewControllers/ColorViewController.swift
+++ ColorizerApp_SS/ViewControllers/ColorViewController.swift
@@ -9,7 +9,9 @@ import UIKit
 
 class ColorViewController: UIViewController {
     
-    
+    // MARK: - IB Outlets
+ 
+
     @IBOutlet var colorizedView: UIView!
     
     @IBOutlet var redLabel: UILabel!
@@ -22,15 +24,15 @@ class ColorViewController: UIViewController {
     @IBOutlet var blueSlider: UISlider!
     @IBOutlet var alpfaSlider: UISlider!
     
-    
+    // MARK: - ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         colorizedView.layer.cornerRadius = 20
-        colorizedView.layer.opacity = 1
         setLabelValues()
         setSliderColors()
     }
     
+    // MARK: - IB Actions
     @IBAction func sliderAction(_ sender: Any) {
         
         setLabelValues()
@@ -44,19 +46,21 @@ class ColorViewController: UIViewController {
         )
     }
     
-    func setLabelValues() {
+    // MARK: - Private functions
+    private func setLabelValues() {
         redLabel.text = String(format: "%.1f", redSlider.value)
         greenLabel.text = String(format: "%.1f", greenSlider.value)
         blueLabel.text = String(format: "%.1f", blueSlider.value)
         alphaLabel.text = String(format: "%.1f", alpfaSlider.value)
     }
     
-    func setSliderColors() {
+    private func setSliderColors() {
         redSlider.minimumTrackTintColor = UIColor(red: CGFloat(redSlider.value), green: 0, blue: 0, alpha: 1)
         greenSlider.minimumTrackTintColor = UIColor(red: 0, green: CGFloat(greenSlider.value), blue: 0, alpha: 1)
         blueSlider.minimumTrackTintColor = UIColor(red: 0, green: 0, blue: CGFloat(blueSlider.value), alpha: 1)
         alpfaSlider.minimumTrackTintColor = UIColor(red: 0, green: 0, blue: 0, alpha: CGFloat(alpfaSlider.value))
     }
+    
     
 }
 
